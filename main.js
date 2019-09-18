c2af5fddb749bc7a4be47c5d5fb643ef8efe3dd8
kkw_10 <kkw_10@naver.com>
2019-09-18 19:28:28 +0900

    기능: helmet설치 및 적용

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const template = require('./lib/template');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const indexRouter= require('./routes/index');
 const topicRouter = require('./routes/topic');
+const helmet = require('helmet');
 
+// middle
+app.use(helmet());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
 
+// coustom middle
 app.get('*', (req, res, next) => {
   fs.readdir('./data', function(error, filelist) {
     req.list = filelist;
@@ -18,13 +21,14 @@ app.get('*', (req, res, next) => {
   })
 })
 
+// routing
 app.use('/', indexRouter)
 app.use('/topic', topicRouter)
 
+// error
 app.use((req, res, next) => {
   res.status(404).send("404 not found...")
 })
-
 app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).send("Somthing broke...")
